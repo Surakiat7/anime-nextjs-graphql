e217d809454ca3ce73bab256c648328b2ce5df07
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-09-17 17:15:42 +0700

    Build fixed

--- src/app/(routes)/recommendation/recommendation.tsx
+++ src/app/(routes)/recommendation/recommendation.tsx
@@ -5,9 +5,7 @@ import NavBar from '@/app/layout/Navbar/Navbar';
 import AnimeCardGrid from '@/app/components/Card/CardAnime';
 import Footer from '@/app/layout/Footer/Footer';
 
-type Props = {};
-
-export default function RecommendationPage({}: Props) {
+export default function RecommendationPage() {
   return (
     <>
       <NavBar />
--- src/app/(routes)/trending/trending.tsx
+++ src/app/(routes)/trending/trending.tsx
@@ -5,9 +5,7 @@ import NavBar from '@/app/layout/Navbar/Navbar';
 import AnimeCardGrid from '@/app/components/Card/CardAnime';
 import Footer from '@/app/layout/Footer/Footer';
 
-type Props = {};
-
-export default function TrendingPage({}: Props) {
+export default function TrendingPage() {
   return (
     <>
       <NavBar />
