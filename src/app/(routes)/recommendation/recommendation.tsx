e3ba9aeee7a4c571119098ab5f1ca3f81856f08d
Surakiat7 <mr.surakiat.ta.62@ubu.ac.th>
2024-09-19 11:56:05 +0700

    Refactor anime recommendation page title

--- src/app/(routes)/recommendation/recommendation.tsx
+++ src/app/(routes)/recommendation/recommendation.tsx
@@ -11,7 +11,7 @@ export default function RecommendationPage() {
       <NavBar />
       <div className="w-full justify-center flex flex-col sm:gap-4 gap-4 px-2 sm:px-4 pt-2 sm:pt-4">
         <h1 className="bg-gradient-to-b from-[#00597B] to-[#47E0E6] inline-block text-transparent bg-clip-text font-bold text-3xl text-center sm:text-left">
-          Anime Recommendations
+          Anime Recommendation
         </h1>
       </div>
       <AnimeCardGrid />
